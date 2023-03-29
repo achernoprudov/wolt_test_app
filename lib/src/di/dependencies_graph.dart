f0808601c4de47f77e37de79a31e39fb6ec7ca8c
Andrei <dinloq@gmail.com>
2023-03-29 16:00:43 +0400

    di graph

---
+++ lib/src/di/dependencies_graph.dart
@@ -0,0 +1,17 @@
+import 'package:wolt_test_task/src/data/location_repository.dart';
+
+class DependenciesGraph {
+  final DataLayer dataLayer;
+
+  DependenciesGraph({
+    required this.dataLayer,
+  });
+}
+
+class DataLayer {
+  final LocationRepository locationRepository;
+
+  DataLayer({
+    required this.locationRepository,
+  });
+}
