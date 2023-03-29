5a877a67d0459c086734eef1fccbe5ee5951e903
Andrei <dinloq@gmail.com>
2023-03-29 16:09:50 +0400

    dio repository

--- lib/src/index.dart
+++ lib/src/index.dart
@@ -1,2 +1,5 @@
 export 'data/location_repository.dart';
+export 'data/restaurants_repository.dart';
 export 'model/location.dart';
+export 'model/restaurant.dart';
+export 'di/dependencies_graph.dart';
